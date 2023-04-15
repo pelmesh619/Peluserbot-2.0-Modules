1999b5e97ed2a4f7bdff4dfeba77ec3f2193b83d
Pelmeska Pelmenov <75571195+pelmesh619@users.noreply.github.com>
2023-04-15 11:16:37 +0300

    Testing new feature

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 from pyrogram import Client, filters
 from core import Module, Author
 
-__version__ = 'v1.2.0-alpha'
+__version__ = 'v1.2.0-beta'
+
+return
 
 module = Module(
     module_id='test',
@@ -64,7 +66,7 @@ async def test_from_me_handler(_, msg):
     await msg.edit(module.get_string('bot_works'))
 
 
-@Client.on_message(filters.command('test', ['!']) & filters.me)
+@Client.on_message(filters.command('fuck', ['!']))
 async def test_from_me_2_handler(_, msg):
-    await msg.edit(module.get_string('fuck'))
+    await msg.reply(module.get_string('fuck'))
 
